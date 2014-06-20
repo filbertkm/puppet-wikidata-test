7af793daa8435c117a296d228937d761b3521e3f
aude <aude.wiki@gmail.com>
2014-06-20 12:59:54 +0000

    improve composer usage

--- manifests/composer.pp
+++ manifests/composer.pp
@@ -2,16 +2,27 @@ class wikidata_test::composer() {
 
     require wikidata_test::mwextensions
 
-    class { 'composer::install_bin':
-        composer_home => '/tmp/composer',
-        install_dir => '/usr/local/bin',
-        source_dir => '/srv/common';
+    define wikidata_test::composer::install_bin() {
+        class { 'composer::install_bin':
+            composer_home => '/tmp/composer',
+            install_dir => '/usr/local/bin',
+            source_dir => '/srv/common';
+        }
     }
 
-    class { 'composer::update':
-        composer_home => '/tmp/composer',
-        install_dir => '/usr/local/bin',
-        target => '/srv/common/extensions/WikidataBuild',
+    define wikidata_test::composer::update(
+        $user
+    ) {
+        class { 'composer::update':
+            composer_home => '/tmp/composer',
+            install_dir => '/usr/local/bin',
+            target => "/srv/common/extensions/${title}",
+            user => $user;
+        }
+    }
+
+    wikidata_test::composer::install_bin { ['WikidataBuild']: }
+    wikidata_test::composer::update { ['WikidataBuild']:
         user => 'mwdeploy'
     }
 
